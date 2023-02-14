54f8b1789458c1106e5f297955f10bd8458fcd37
ClarXo <cheonaru@gmail.com>
2023-02-15 00:52:05 +0900

    initial commit

---
+++ Assets/Undead Survivor/Scripts/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+  public Vector2 inputVec;
+  public float speed;
+
+  Rigidbody2D rigid;
+
+  // 초기화용 함수(1회 실행)
+  void Awake()
+  {
+    rigid = GetComponent<Rigidbody2D>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // inputVec.x = Input.GetAxis("Horizontal");   
+    // inputVec.y = Input.GetAxis("Vertical");   
+
+
+    // GetAxisRaw: Returns the value of the virtual axis identified by axisName with no smoothing filtering applied.
+    inputVec.x = Input.GetAxisRaw("Horizontal");
+    inputVec.y = Input.GetAxisRaw("Vertical");
+  }
+
+  // 물리 연산 프레임마다 호출
+  void FixedUpdate()
+  {
+    // 1. 힘을 통한 구현
+    // rigid.AddForce(inputVec);
+
+    // 2. 속도를 통한 구현
+    // rigid.velocity = inputVec;
+
+    // 3. 위치를 통한 구현
+    // rigid.MovePosition(rigid.position + inputVec);
+
+    // normalized: Returns this vector with a magnitude of 1 (Read Only).
+    // fixedDeltaTime: The interval in seconds at which physics and other fixed frame rate updates (like MonoBehaviour's MonoBehaviour.FixedUpdate) are performed.
+    Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+    rigid.MovePosition(rigid.position + nextVec);
+  }
+}
